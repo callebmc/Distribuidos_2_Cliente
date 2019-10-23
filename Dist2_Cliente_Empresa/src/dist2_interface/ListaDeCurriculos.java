b8f9dd6ae04814486abe285b3a457d846f29b246
Ana Y <anayanaze@hotmail.com>
2019-10-23 03:29:41 -0300

    adicoinando

---
+++ Dist2_Cliente_Empresa/src/dist2_interface/ListaDeCurriculos.java
@@ -0,0 +1,47 @@
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import dist2_interface.Curriculo;
+
+public class ListaDeCurriculos implements Serializable{
+    private ArrayList<Curriculo> listaCurriculos;
+    
+    public ListaDeCurriculos(){
+        this.listaCurriculos = new ArrayList();
+    }
+    
+    
+    // Adiciona novo currículo a Lista
+    public boolean adicionar (Curriculo curriculo){
+        if (listaCurriculos.contains(curriculo)){
+            System.out.println("Currículo já cadastrado no sistema");
+            return false;
+        }
+        else {
+        listaCurriculos.add(curriculo);
+        System.out.println("Currículo Cadastrado com sucesso \n");
+        return true;
+       }
+      
+    }
+    
+    
+    //Consulta a partir dos filtros
+    public List<Curriculo> consultar (String area){
+        List<Curriculo> curriculosFiltered;
+        
+        curriculosFiltered = new ArrayList();
+        for (Curriculo curriculo : listaCurriculos){
+            if (curriculo.getArea() == area){
+                curriculosFiltered.add(curriculo);
+            }
+        }
+        
+        return curriculosFiltered;
+    }
+    
+    
+    
+    
+}
